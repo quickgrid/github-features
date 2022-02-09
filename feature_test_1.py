89c362a815f807699da9cf836a6395fe75bc843e
Asif Ahmed <public.quickgrid@gmail.com>
2022-02-09 21:54:13 +0600

    Update feature_test_1.py

--- feature_test_1.py
+++ feature_test_1.py
@@ -1,10 +1,16 @@
-# Intentionally write bad code for linter.
+"""Module docstring.
+
+Intentionally write bad code for linter and try to fix.
+"""
+
 def abc():
+    """function dogstring.
+    """
     print('s')
 
-    
 def hello():
+    """function dogstring.
+    """
     print('world')
-    
     
 print(abc())
